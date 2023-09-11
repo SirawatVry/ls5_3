1f08f4cfb50b9751fa39e590c0fe2f64eb91d620
SirawatVry <139094315+SirawatVry@users.noreply.github.com>
2023-09-11 11:27:25 +0700

    Update 6.cpp

--- 6.cpp
+++ 6.cpp
@@ -2,27 +2,27 @@
 #include <string>
 #include <iomanip>
 using namespace std;
-float update_balance(char command,float balance);
+float update_balance(char command,float balance,float dollar);
 int main()
 {	char command;
-	float balance = 10000;
+	float dollar,balance = 10000;
 	do{
 	cout << "Exit with 0\n"<<fixed<<setprecision(2)<<"Your balance = "<< balance <<endl;
 	cout << "Input command (1 or withdraw, 2 deposit):";
 	cin >> command;
-	balance = update_balance(command,balance);
+	if(command == '0')
+		break;
+	cout << "Input amnout : ";
+	cin >> dollar;
+	balance = update_balance(command,balance,dollar);
 	}while (command!='0');
 }
-float update_balance(char command,float balance)
+float update_balance(char command,float balance,float dollar)
 {	
-	float dollar;
+
 	if(command=='1'){
-		cout << "Input amnout : ";
-		cin >> dollar;
 		balance = balance - dollar;
 	}else if(command=='2'){
-		cout << "Input amnout : ";
-		cin >> dollar;
 		balance = balance + dollar;}
 	return (balance);
 }
